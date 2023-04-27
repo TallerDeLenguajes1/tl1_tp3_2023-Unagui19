f6a85bef29cbec7e74581cbe1603d52ac1bd259b
Unagui19 <francorr199@gmail.com>
2023-04-27 01:09:48 -0300

    Punto 3 modificado agregado, ademas de corregir errores de ortografia

--- punto3Modificado.c
+++ punto3Modificado.c
@@ -1,6 +1,9 @@
 /* 3) Escriba un programa que solicite 5 nombres, los cargue en un vector de punteros char y una
  vez cargados los liste por pantalla (Todo implementando reserva dinámica de memoria) (Lea
- las notas 1 y 2)*/
+ las notas 1 y 2)
+ 
+ 5)Modifique el ejercicio nro 3 para que, en lugar de ingresar un número fijo de nombres, el
+usuario pueda indicar previamente la cantidad de nombres que ingresará a continuación.*/
 
 
 
@@ -11,12 +14,17 @@
 
 int main()
 {
+    int cantNombres;
+    printf("Cantidad de nombres a ingresar: ");
+    scanf("%d",&cantNombres);
+    fflush(stdin);
+
+    char **nombres;
+    nombres=(char**) malloc(sizeof(char*) * cantNombres);
+
     char *Buff; //declaro variable auxiliar
-    char *nombres[5];
-
     Buff=(char*) malloc(sizeof(char) * 100);   
-
-    for (int i = 0; i < 5; i++)//para cargar el vector nombres
+    for (int i = 0; i < cantNombres; i++)//para cargar el vector nombres
     {
     printf("Ingrese el nombre numero %d\n\r",i+1);
     gets(Buff);
@@ -26,7 +34,7 @@ int main()
 
     printf("\nLos nombres cargados son: \n\r");
 
-    for (int j = 0; j < 5; j++)//para mostrar el vector nombres
+    for (int j = 0; j < cantNombres; j++)//para mostrar el vector nombres
     {
         puts(nombres[j]);
     }
@@ -35,7 +43,7 @@ int main()
     {
         free(nombres[j]);
     }
-
+    free(nombres);
     free(Buff);
 
     return 0;
